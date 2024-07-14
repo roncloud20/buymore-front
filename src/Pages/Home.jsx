0ab44584145f80324bbe8bd2e864770f4aba077f
Olugbenga Raymond <olugbengaraymond20@gmail.com>
2024-07-14 15:55:09 +0100

    updating the home page

--- src/Pages/Home.jsx
+++ src/Pages/Home.jsx
@@ -1,13 +1,61 @@
-import Header from '../Components/Header';
-import ProductList from '../Components/ProductList';
+import Header from "../Components/Header";
+import { useEffect, useState } from 'react';
+import ProductList from "../Components/ProductList";
 
-export default function Home () {
+export default function Home() {
+    const [data, setData] = useState([]);
+
+    const fetchData = async () => {
+        try {
+            let response = await fetch("http://localhost:8000/api/list");
+            let result = await response.json();
+            setData(result);
+        } catch (error) {
+            console.error("Error fetching data:", error);
+        }
+    };
+    useEffect(() => {
+        fetchData();
+    }, []);
+
+    console.log(data);
+
+    const handleSearch = (query)=>{
+        if (!query) {
+            fetchData();
+        } else {
+            const filtered  = data.filter(product =>
+                (product.product_name ?? "").toLowerCase().includes(query.toLowerCase()) || (product.category ?? "").toLowerCase().includes(query.toLowerCase())
+            );
+            setData(filtered);
+            if (filtered.length === 0) {
+                alert("Search Not Found");
+            }
+        }
+    }
     return (
         <>
-            <Header/>
-            <h1>Welcome To The Home Page</h1>
-            <ProductList/>
-            
+            <Header onSearch={handleSearch}/>
+            <h1>Dashboard Page</h1>
+            <ProductList data={data}/>
         </>
     )
 }
+
+
+
+
+
+// import Header from '../Components/Header';
+// import ProductList from '../Components/ProductList';
+
+// export default function Home () {
+//     return (
+//         <>
+//             <Header/>
+//             <h1>Welcome To The Home Page</h1>
+//             <ProductList/>
+            
+//         </>
+//     )
+// }
